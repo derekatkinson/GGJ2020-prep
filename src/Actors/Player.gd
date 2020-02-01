77570bd9c1e3d259907dc75b1770b41663449b6d
Elad Karni <elad.karni@gmail.com>
2020-02-01 00:04:38 -0500

    Double Jump & Dashing Added

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -3,11 +3,15 @@ extends Actor
 onready var player_sprite = get_node("player")
 onready var player_anim = get_node("AnimationPlayer")
 
+export var MAX_JUMPS := 1
+var CURRENT_JUMP := 0
+
 func _physics_process(delta: float) -> void:
 	var direction: = get_direction()
 	velocity = calculate_move_velocity(velocity, direction, speed)
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
+	if Input.is_action_just_pressed("dash"):
+		dash()
 	
 	
 	# Animation
@@ -29,9 +33,19 @@ func _physics_process(delta: float) -> void:
 	
 func get_direction() -> Vector2:
 	return Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), player_jump()
 	)
+
+func player_jump():
+	MAX_JUMPS = PlayerData.jump_count
+	if is_on_floor():
+		CURRENT_JUMP = 0
+	if Input.is_action_just_pressed("jump") and MAX_JUMPS > CURRENT_JUMP:
+		CURRENT_JUMP += 1
+		return -1.0
+	else:
+		return 1.0
+
 	
 func calculate_move_velocity(
 		linear_velocity: Vector2,
@@ -44,3 +58,10 @@ func calculate_move_velocity(
 	if direction.y == -1.0:
 		new_velocity.y = speed.y * direction.y
 	return new_velocity
+
+func dash():
+	speed.x = 1000
+	$Timer.start()
+
+func _on_Timer_timeout() -> void:
+	speed.x = 100
--- src/AutoLoaders/PlayerData.gd
+++ src/AutoLoaders/PlayerData.gd
@@ -2,7 +2,11 @@ extends Node
 
 signal score_updated
 
-var upgrade: = false
+var upgrade := false
+var jump_count := 1
 
 func toggle_upgrade() -> void:
 	upgrade = !upgrade
+
+func enable_double_jump() -> void:
+	jump_count = 2
